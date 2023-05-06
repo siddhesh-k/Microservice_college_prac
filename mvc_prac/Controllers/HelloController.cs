c6564ffcf7da0ebddca461e5cd3ae40d89877211
siddhesh khadapkar <siddhesh.khadapkar@hertengroup.com>
2023-05-07 02:28:34 +0530

    Add project files.

---
+++ mvc_prac/Controllers/HelloController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Xml.Linq;
+using mvc_prac.Models;
+
+namespace mvc_prac.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "hello world";
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return Content("Content hello world") ;
+        //}
+
+        public IActionResult Student() {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "ABC",
+                Gender = "male",
+            };
+            return View(student);
+        }
+
+
+    }
+}
---
+++ mvc_prac/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace mvc_prac.Models
+{
+    public class Student
+    {
+        
+        public int Id { get; set; }
+
+        public string Name { get; set; }    
+        public string Gender { get; set; }
+        }
+    
+}
